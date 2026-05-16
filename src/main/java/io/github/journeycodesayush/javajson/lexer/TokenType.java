9e4601774d2617a045511aec0e3b2fe6e76d792b
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-05-16 11:00:03 +0530

    feat(lexer): add JSON TokenType enum

---
+++ src/main/java/io/github/journeycodesayush/javajson/lexer/TokenType.java
@@ -0,0 +1,21 @@
+package io.github.journeycodesayush.javajson.lexer;
+
+public enum TokenType {
+    // Single character tokens
+    LEFT_CURLY_BRACE,
+    RIGHT_CURLY_BRACE,
+    LEFT_BRACKET,
+    RIGHT_BRACKET,
+    COLON,
+    COMMA,
+
+    // String, boolean, number
+    STRING,
+    TRUE,
+    FALSE,
+    NUMBER,
+    NULL,
+
+    // End of File
+    EOF
+}
