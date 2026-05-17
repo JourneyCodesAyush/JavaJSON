48ecd1bbf91b6d0a45f1844716f65a75538be04a
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-05-17 23:06:44 +0530

    feat(query): add JsonQuery to return JsonValue after querying the JSON

---
+++ src/main/java/io/github/journeycodesayush/javajson/query/JsonQuery.java
@@ -0,0 +1,28 @@
+package io.github.journeycodesayush.javajson.query;
+
+import java.util.List;
+
+import io.github.journeycodesayush.javajson.parser.JsonValue;
+
+public class JsonQuery {
+    public static JsonValue get(JsonValue root, List<PathSegment> segments) {
+        JsonValue current = root;
+        for (PathSegment segment : segments) {
+            switch (segment) {
+                case PathSegment.Key k -> {
+                    if (current instanceof JsonValue.JsonObject object) {
+                        System.out.println(k.name());
+                        current = object.members().get(k.name());
+                    }
+                }
+                case PathSegment.Index i -> {
+                    if (current instanceof JsonValue.JsonArray array) {
+                        System.out.println(i.index());
+                        current = array.elements().get(i.index());
+                    }
+                }
+            }
+        }
+        return current;
+    }
+}
